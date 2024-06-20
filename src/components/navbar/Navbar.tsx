9a45e88ca36857c0cf5de3e73fad67a015e08392
UkandieSunday <ukandieudo202@gmail.com>
2024-06-20 20:03:39 +0100

    set up the react project

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./navbar.scss";
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navLeft">
+        <img src="/images/ukandie1.jpg" alt="logo" className="img" />
+      </div>
+      <div className="navRight"></div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/navbar/navLinks.ts
@@ -0,0 +1,23 @@
+const navLinks = [
+  {
+    id: "1",
+    link: "about",
+  },
+  {
+    id: "1",
+    link: "service",
+  },
+
+  {
+    id: "2",
+    link: "resume",
+  },
+  {
+    id: "3",
+    link: "projects",
+  },
+  {
+    id: "4",
+    link: "contact",
+  },
+];
