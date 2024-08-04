288227cc88e062b0df0b6d29a0404451f4afeafd
UkandieSunday <ukandieudo202@gmail.com>
2024-08-04 16:56:55 +0100

    update

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -61,7 +61,7 @@ const Banner = () => {
                 <div className="cvWrapper">
                   <a
                     target="_blank"
-                    href="/images/ukandie-sunday.cvT.pdf"
+                    href="/images/ukandie-sunday.cvTE.pdf"
                     download
                     className="cv"
                   >
